e1a25a61ac1eb05a6d83556bb8db4a163bd6f4b4
Ari Fiorino <arifiorino@gmail.com>
2021-07-19 20:52:12 -0400

    New find square

--- find_square.py
+++ find_square.py
@@ -5,7 +5,7 @@ import sys, scipy.optimize
 target_image = draw_config([0.006,0.01,0,0,0.01,0,0,0.01,0,0,0.01,0,0])
 target_O = score_image(target_image)
 
-N_test = 10
+N_test = 20
 test_E = np.random.rand(N_test, 13) * 0.01
 test_O = np.zeros((N_test, 8))
 for i in range(N_test):
@@ -14,19 +14,32 @@ for i in range(N_test):
   O = score_image(im)
   test_O[i] = O
 
+deps = \
+[[1,0,0,0,0,0,0,0,0,0,1,1,1],
+ [1,1,1,0,0,0,0,0,0,0,1,1,1],
+ [0,1,1,1,1,0,0,0,0,0,0,0,0],
+ [0,0,0,1,1,1,0,0,0,0,0,0,0],
+ [0,0,0,0,1,1,1,1,0,0,0,0,0],
+ [0,0,0,0,0,0,1,1,1,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,1,1,1,0]]
+
 K = np.zeros((8, 13))
 for i in range(8):
-  Es = np.zeros((N_test, 13))
   Os = test_O[:,i]
-  res = scipy.optimize.lsq_linear(Es, Os)
+  bounds = (np.array(deps[i])+0.000001)*1000000
+  #print(bounds)
+  res = scipy.optimize.lsq_linear(test_E, Os, bounds=(-bounds,bounds))
+  #res = scipy.optimize.lsq_linear(test_E, Os)
   Ks = res['x']
   for j in range(13):
     K[i,j]=Ks[j]
+print(K)
 
 res = scipy.optimize.lsq_linear(K, target_O, bounds=(0,np.inf))
 E = res['x']
+print('expected',res['fun'])
 im = draw_config(E)
 im.show()
 O = score_image(im)
 print(O - target_O)
-print(O)
